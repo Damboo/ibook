6c6bbe8cb78befc5ba1e126a8d11ba30f0bb7d17
宫磊 <gong.lei@trs.com.cn>
2019-03-13 15:38:24 +0800

    修改ibook-service模块打包文件名称

---
+++ ibook-service/src/main/java/com/trs/ibook/service/api/BookFrontShowAPI.java
@@ -0,0 +1,19 @@
+package com.trs.ibook.service.api;
+
+import io.swagger.annotations.Api;
+
+/**
+ * Title:【电子书前端展示相关服务】API
+ * Description: swagger接口文档
+ * Copyright: 2019 拓尔思信息技术股份有限公司 版权所有.保留所有权
+ * Company: 拓尔思信息技术股份有限公司(TRS)
+ * Project: ibook
+ * Author: RayeGong
+ * Create Time: 2019-03-13 15:30
+ */
+@Api(tags = "BookFrontShow", description = "电子书前端展示相关服务")
+public interface BookFrontShowAPI {
+
+
+
+}
